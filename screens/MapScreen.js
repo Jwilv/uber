087e540455620300521e06fe64f4affab2f9f1bd
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-01 19:26:38 -0300

    MapScreen StyleSheet

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { Platform } from 'react-native'
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView style={[tw`bg-white h-full`, styles.androidSafeArea]}>
+      <View style={tw`p-5`}>
+        <Text>MapScreen</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default MapScreen
+
+const styles = StyleSheet.create({
+    androidSafeArea: {
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    }
+  })
