b360f592dfc6e61f026a1bdcf6c38cb5814c43af
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-01 17:49:24 -0300

    SafeAreaProvider

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-   <Provider store={store}>
-    <HomeScreen /> 
-   </Provider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
