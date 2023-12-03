ce03865f5fad61dd45bb5b6c26f251c0fda46de7
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-03 17:27:27 -0300

    decode

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,14 +3,16 @@ import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Platform } from 'react-native'
 import MapView from 'react-native-maps';
+import polyline from '@mapbox/polyline';
 
 const MapScreen = () => {
 
+  const decodedGeometry = polyline.decode("sxxwFfkpbMmmX`np@knE~ifDat[jc{@tfPhc`GuvPz`i@fo@vk|AjlWra}@{uNf_c@bdKjzd@ab_@xvyA_lKpb}B`_@|}|FnpMfu|@giXn_^}kAv`sBktWzmnAz|IlxqB_vF|wdAook@~whCajFplcFazWni|@{BfecGxbc@x}eBqiAl~jCpqQzi`Bj{`@gg@bznA~y{A`en@teSpqWjn`@wvf@rdoE");
   return (
     <SafeAreaView style={[tw`bg-white h-full`, styles.androidSafeArea]}>
       <View style={[tw`mt-5`, styles.page]}>
-        <MapView 
-        style={styles.map}
+        <MapView
+          style={styles.map}
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
