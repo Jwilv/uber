af6156f089e7a2ff29a3ab8938c221cbd72e32e4
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-03 18:20:55 -0300

    cors

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,35 @@
 import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Platform } from 'react-native'
 import MapView from 'react-native-maps';
 import polyline from '@mapbox/polyline';
+import MapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+
+const mapboxClient = MapboxGeocoding({ accessToken: 'sk.eyJ1IjoibXJ3aWx2IiwiYSI6ImNscHB4ejBwejAybnEya2s4ZjV1Z2NnZjIifQ.uN1cXDshYEetXTCk69CnEQ' });
+
+// sk.eyJ1IjoibXJ3aWx2IiwiYSI6ImNscHB4ejBwejAybnEya2s4ZjV1Z2NnZjIifQ.uN1cXDshYEetXTCk69CnEQ
 
 const MapScreen = () => {
+
+  const [cors, setCors] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
+
+  const response = async () => {
+    const response = await mapboxClient.forwardGeocode({
+      query: 'temperley',
+      types: ['place'],
+    }).send();
+
+    console.log('result :');
+    console.log(response.body.features[0].geometry);
+  }
+
+  response()
+
+
 
   const decodedGeometry = polyline.decode("sxxwFfkpbMmmX`np@knE~ifDat[jc{@tfPhc`GuvPz`i@fo@vk|AjlWra}@{uNf_c@bdKjzd@ab_@xvyA_lKpb}B`_@|}|FnpMfu|@giXn_^}kAv`sBktWzmnAz|IlxqB_vF|wdAook@~whCajFplcFazWni|@{BfecGxbc@x}eBqiAl~jCpqQzi`Bj{`@gg@bznA~y{A`en@teSpqWjn`@wvf@rdoE");
   return (
@@ -14,11 +38,12 @@ const MapScreen = () => {
         <MapView
           style={styles.map}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: -34.775882,
+            longitude: -58.39712,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
+          
         />
       </View>
     </SafeAreaView>
