9ae19f53e65fb834cd41efc99cdf354584669772
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-11 19:26:33 -0300

    mapbox service

--- components/SearchLocation.js
+++ components/SearchLocation.js
@@ -1,34 +1,21 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import SearchResults from './SearchResults'
-import MapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+import { getLocationByName } from '../services/mapbox'
 
 
-const mapboxClient = MapboxGeocoding({ accessToken: 'sk.eyJ1IjoibXJ3aWx2IiwiYSI6ImNscHB4ejBwejAybnEya2s4ZjV1Z2NnZjIifQ.uN1cXDshYEetXTCk69CnEQ' });
 
 export default function SearchLocation() {
 
     const [inputValue, setInputValue] = useState('')
 
-    const response = async () => {
-        const response = await mapboxClient.forwardGeocode({
-            query: inputValue,
-            types: ['place'],
-        }).send();
 
-        console.log('result :');
-        console.log(response.body.features[0].geometry);
-    }
-
-
-
-    const handleSubmit = () => {
-        response()
+    const handleSubmit = async() => {
+        const data = await getLocationByName('temperley');
     }
 
     const handleChange = (e) => {
         setInputValue(e.nativeEvent.text);
-        console.log(e.nativeEvent.text);
     }
 
     return (
---
+++ services/mapbox.js
@@ -0,0 +1,22 @@
+import MapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+
+
+const mapboxClient = MapboxGeocoding({ accessToken: 'sk.eyJ1IjoibXJ3aWx2IiwiYSI6ImNscHB4ejBwejAybnEya2s4ZjV1Z2NnZjIifQ.uN1cXDshYEetXTCk69CnEQ' });
+
+export const getLocationByName = async (location = '') => {
+
+    const response = await mapboxClient.forwardGeocode({
+        query: location,
+        types: ['place'],
+    }).send();
+
+    return response.body.features.map(
+        ({ place_name, text, center, id }) => ({
+                place_name,
+                text,
+                center,
+                id
+        })
+    );
+
+}
