0699b56e3c0426d3a727bd24f66e1279ad9772a3
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-11 19:56:15 -0300

    locationSlice

---
+++ sclices/locationsSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    locations: [],
+}
+
+const locationSlice = createSlice({
+    initialState,
+    name: 'searchLocations',
+    reducers: {
+        setLocations: (state, action) => ({
+            ...state,
+            [state.locations]: action.payload,
+        }),
+        deleteLocations: (state) => ({
+            ...state,
+            [state.locations]: initialState.locations,
+        })
+    }
+});
+
+export const { setLocations, deleteLocations } = locationSlice.actions;
+
+export const selectLocations = (state) => state.searchLocations.locations;
+
+export default locationSlice.reducer
