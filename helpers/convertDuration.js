d9a0d7466aadc388ba546478691705bf38f554e6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-17 21:13:58 -0300

    convertDuration

--- helpers/convertDuration.js
+++ helpers/convertDuration.js
@@ -1,4 +1,12 @@
 
 export const convertDuration = (duration) => {
-    return duration / 3600
+    const time = duration / 3600;
+    const timeInHours = Math.round(time * 100) / 100;
+
+    if (timeInHours > 1) {
+        return `${timeInHours} hr`;
+    } else {
+        const timeInMinutes = Math.round(time * 60 * 100) / 100;
+        return `${timeInMinutes} min`;
+    }
 }
