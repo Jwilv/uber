f40ee9afb8cd0ac0c396558be900c99682baacb3
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-18 22:01:22 -0300

    ExitButton

--- components/ExitButton.js
+++ components/ExitButton.js
@@ -1,11 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export const ExitButton = ({ onPress }) => {
 
     return (
-        <View>
-            <Text onPress={onPress}>ExitButton</Text>
+        <View style={{
+            position: 'absolute',
+            width: 40,
+            height: 30,
+            overflow: 'hidden',
+            backgroundColor: 'black',
+            display: 'flex',
+            paddingLeft: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <TouchableOpacity>
+                <AntDesign
+                    name="arrowleft"
+                    size={24}
+                    color="white"
+                    onPress={onPress}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
